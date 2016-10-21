c1093b88b4bb203cebb4129fb89f8cad97f56202
Giandre Acosta <gacosta@g.clemson.edu>
2016-10-21 08:20:27 -0400

    Update rpiCardScan.py

--- Software/scan_key/rpiCardScan.py
+++ Software/scan_key/rpiCardScan.py
@@ -5,13 +5,12 @@ from select import select
 
 def RPICardScan():
 	keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
-	#keys = "X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
 	dev = InputDevice('/dev/input/by-id/usb-RFIDeas_USB_Keyboard-event-kbd')
 	arr = []
 	while True:
-		r,w,x = select([dev], [], [])
+		r, w, x = select([dev], [], [])
 		for event in dev.read():
-			if event.type==1 and event.value==1:
+			if event.type == 1 and event.value == 1:
 				arr.append(keys[event.code])
 				#print( keys[ event.code ] )
 #		print (arr)
@@ -19,7 +18,7 @@ def RPICardScan():
 			break
 
 	length = len(arr)
-	join1 = arr[:length-1]
+	join1 = arr[:length - 1]
 	join1 = ''.join(join1)
 	return (join1)
 
