2131aa6cd5038a7a907eac6bbdb28713064f14b5
r3cebarnett <mauralanbarn@gmail.com>
2016-11-10 20:03:00 -0500

    Update keypad.py

--- Software/scan_key/keypad.py
+++ Software/scan_key/keypad.py
@@ -16,8 +16,8 @@ class keypad():
 				["*",0,"#"]
 			]
 
-			self.ROW         = [12,16,21,20]
-			self.COLUMN      = [23,24,25]
+			self.ROW         = [12,25,24,23]
+			self.COLUMN      = [20,21,16]
 
 		elif columnCount is 4:
 			self.KEYPAD = [
