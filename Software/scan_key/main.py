026d4ef8a08030e6abbed60c040e2967fdddb7e6
Owen Phillips <ophilli@clemson.edu>
2017-01-12 19:23:34 -0500

    Correct Auth Details for Demo

--- Software/scan_key/main.py
+++ Software/scan_key/main.py
@@ -18,12 +18,11 @@ import os
 if __name__ == '__main__':
 
 	# connecting to the SQL Server and Database
-#	cnx = pymysql.connect(user='gacosta', password='9)q=2d-dz[4g', host='sbxmysql.clemson.edu', database='gacosta', autocommit=True)
-#	cnx = pymysql.connect(user='CATS', password='CATS', host='CATS-SQL.local', database='CATS', autocommit=True)
-	cnx = pymysql.connect(user='CATS', password='****', host='192.168.0.148', database='CATS', autocommit=True)
-	
+#	cnx = pymysql.connect(user='CATS', password='****', host='sbxmysql.clemson.edu', database='****', autocommit=True)
+	cnx = pymysql.connect(user='CATS', password='CATS', host='CATS-SQL.local', database='CATS', autocommit=True)
+
 	cursor = cnx.cursor()
-	
+
 	# select the first and last name, the CUID, their Card String, and PIN from
 	# the database
 	cursor.execute("SELECT firstName, lastName, CUID, t1String, Pin FROM USER")
@@ -33,11 +32,11 @@ if __name__ == '__main__':
 
 	# initialize the keypad and important variables
 #	count = 1
-	
+
 #	for row in data:
 #		print (str(count) + ".\t" + str(row[0]) + '\t' + str(row[1]) + '\t' + str(row[2]) + ' \t' + str(row[3]) + '\t' + str(row[4]))
 #		count = count + 1
-	
+
         # initialize the keypad and important variables
 	kp = keypad()
 	ID = None; readpad = 0; join = 0; character = 0; length = 0;
@@ -76,5 +75,5 @@ if __name__ == '__main__':
 				cnx.close()
 				sys.exit(0)
 		# turn off the power if it's been turned on
-		TurnPowerOff()		
-	
+		TurnPowerOff()
+
