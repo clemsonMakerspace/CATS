cd88630391c89c7dc34ec9c427844c5fce2a3550
r3cebarnett <mauralanbarn@gmail.com>
2017-02-06 23:35:01 -0500

    Added second test!

--- Software/scan_key/nose.py
+++ Software/scan_key/nose.py
@@ -3,15 +3,19 @@ import configparser
 import pymysql
 import os
 from AuthenticationSQL import *
+from nose.tools import *
 
-def testAuthSQL():
+def testMachineAuth():
     #machineAuth test
     #implement cursor
-    id1 = "2350193059" #Add working id
-    id2 = "2350207754" #Add nonworking id
+
 
     config = configparser.RawConfigParser()
     config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.cfg'))
+
+    id1 = config.get('_test', 'id1') #Add working id
+    id2 = config.get('_test', 'id2') #Add nonworking id
+
     cnx = pymysql.connect(user = config.get('_sql', 'username'),\
                             password = config.get('_sql', 'password'),\
                             host = config.get('_sql', 'hostname'),\
@@ -21,3 +25,22 @@ def testAuthSQL():
 
     assert(machineAuth(id1, cursor),True)
     assert(machineAuth(id2, cursor),False)
+
+def testAuth():
+    config = configparser.RawConfigParser()
+    config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.cfg'))
+
+    id1 = config.get('_test', 'id1')
+    id2 = config.get('_test', 'id2')
+    pin1 = config.get('_test', 'pin1')
+    pin2 = config.get('_test', 'pin2')
+
+    cnx = pymysql.connect(user = config.get('_sql', 'username'),\
+                            password = config.get('_sql', 'password'),\
+                            host = config.get('_sql', 'hostname'),\
+                            database = config.get('_sql', 'database'),\
+                            autocommit = True)
+    cursor = cnx.cursor()
+
+    assert(auth(id1,pin1,cursor), True)
+    assert(auth(id2,pin2,cursor), False)
