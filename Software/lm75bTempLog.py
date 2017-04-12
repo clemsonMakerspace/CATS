50a1b4de2d0e160c3e58b69ad1ff3ae1f9205532
Owen Phillips <ophilli@clemson.edu>
2017-04-12 06:04:10 +0000

    Draft Logging Temperature

--- Software/lm75bTempLog.py
+++ Software/lm75bTempLog.py
@@ -1,5 +1,8 @@
 import smbus
 import datetime
+import logging
+
+logging.basicConfig(filename='temp.log', level=logging.INFO)
 
 bus = smbus.SMBus(1) # found via sudo i2cdetect -y 0 or -y 1
 
@@ -10,9 +13,9 @@ TEMP_REGISTER = 0x00 # found on datasheet
 
 data = bus.read_i2c_block_data(DEVICE_ADDRESS, TEMP_REGISTER)
 
-print("Time:", datetime.datetime.now())
-print("Temp:", ((data[0] << 8 | data[1]) >> 5) * 0.125)
-print()
+logging.info("UTC: %s", datetime.datetime.now())
+logging.info("Temp: %.3f", ((data[0] << 8 | data[1]) >> 5) * 0.125)
+
 # data[0] is most significant byte
 # data[1] is least significant byte
 # first 5 bits of LSB are 0 and should be ignored
